7407bdf7368bf4858f513cf0bfb05d97639ad04e
ArtemYemtsev <artem.y.solardigital@gmail.com>
2025-06-11 10:59:19 +0300

    Changed js code

--- RIEL/script.js
+++ RIEL/script.js
@@ -18,36 +18,39 @@ document.addEventListener('DOMContentLoaded', () => {
   document.head.appendChild(noscript);
 
   if (typeof window.gsap === "undefined") document.documentElement.classList.add("gsap-not-found");
-	gsap.registerPlugin(ScrollTrigger, SplitText);
 
-  document.querySelectorAll("[data-word-reveal='true']").forEach((text) => {
-    const split = SplitText.create(text, {
-      type: "words, chars",
-      mask: "words",
-      wordsClass: "word",
-      charsClass: "char",
+  document.fonts.ready.then(() => {
+    gsap.registerPlugin(ScrollTrigger, SplitText);
+
+    document.querySelectorAll("[data-word-reveal='true']").forEach((text) => {
+      const split = SplitText.create(text, {
+        type: "words, chars",
+        mask: "words",
+        wordsClass: "word",
+        charsClass: "char",
+      });
+
+      console.log(split);
+
+      if (split.words.length) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: text,
+            start: "top bottom",
+            end: "top 80%",
+            toggleActions: "none play none reset",
+          },
+        });
+      
+        tl.from(split.words, {
+          yPercent: 110,
+          delay: 0.2,
+          duration: 0.8,
+          stagger: { amount: 0.5 },
+        });
+      }
+
+      gsap.set(text, { visibility: "visible" });
     });
-
-    console.log(split);
-
-    if (split.words.length) {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: text,
-          start: "top bottom",
-          end: "top 80%",
-          toggleActions: "none play none reset",
-        },
-      });
-    
-      tl.from(split.words, {
-        yPercent: 110,
-        delay: 0.2,
-        duration: 0.8,
-        stagger: { amount: 0.5 },
-      });
-    }
-
-    gsap.set(text, { visibility: "visible" });
   });
 });
